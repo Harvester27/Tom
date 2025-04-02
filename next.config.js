756cc4bb57687a6328077cb5f08ba0f402612dc2
Harvester27 <stepanovsky21@gmail.com>
2025-04-03 01:22:18 +0200

    Remove rewrites from next.config.js

--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const nextConfig = {
     domains: ['tom-ten-kappa.vercel.app'],
     unoptimized: true,
   },
-  // Zajistíme, že statické soubory budou správně servírované
-  async rewrites() {
-    return [
-      {
-        source: '/Images/:path*',
-        destination: '/Images/:path*',
-      },
-    ];
-  },
   // Nastavení pro statické soubory
   async headers() {
     return [
